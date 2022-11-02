91645595ed588387937160a2f127900d49af7771
rayanti <rayanticlimatic@gmail.com>
2022-11-02 15:16:50 +0900

    delete context menu support

--- add_to_context_menu.py
+++
@@ -1,7 +0,0 @@
-keyVal = r'Directory\Background\shell'
-try:
-    key = OpenKey(HKEY_CLASSES_ROOT, keyVal, 0, KEY_ALL_ACCESS)
-except:
-    key = CreateKey(HKEY_CLASSES_ROOT, keyVal)
-SetValueEx(key, "Start Page", 0, REG_SZ, "https://www.blog.pythonlibrary.org/")
-CloseKey(key)
