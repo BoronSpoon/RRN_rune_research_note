9f64222b6792bf68235da3ea8fea1e270ad95584
rayanti <rayanticlimatic@gmail.com>
2022-11-03 18:49:18 +0900

    update pypi version

--- vern/__init__.py
+++ vern/__init__.py
@@ -1,7 +1,7 @@
 from .parse_all import *
 
 __copyright__    = 'Copyright (C) 2022 BoronSpoon'
-__version__      = '1.0.0'
+__version__      = '1.0.1'
 __license__      = 'MIT License'
 __author__       = 'boronspoon'
 __author_email__ = 'rayanticlimactic@gmail.com'
