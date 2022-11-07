415f3a8e73de1cb1108db8be1c23d87d1e5de52e
rayanti <rayanticlimatic@gmail.com>
2022-11-07 14:13:16 +0900

    use latex for plot label in vsm

--- vern/parse_vsm.py
+++ vern/parse_vsm.py
@@ -10,7 +10,7 @@ def read_files(input_path, csv_path, plot_path, hist_path, interactive=False):
         
     df = pd_read_csv(
         filename=input_path, encoding="cp932", sep=",", header_count=28, 
-        names_old=["magnetic field (Oe)", "magnetization (erg/cm^3)"], names_new=["magnetic field (Oe)", "magnetization (erg/cm^3)"], unit_conversion_coefficients=[1, 1]
+        names_old=["magnetic field (Oe)", r"$magnetization (erg/cm^{3})$"], names_new=["magnetic field (Oe)", r"magnetization (erg/cm^{3})"], unit_conversion_coefficients=[1, 1]
     )
     df.to_csv(csv_path, index=False)
     df = remove_outlier(df)
