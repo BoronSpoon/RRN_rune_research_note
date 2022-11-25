b942b545a45b1091c9c4304188e7eea8ab4e7913
rayanti <rayanticlimatic@gmail.com>
2022-11-25 13:22:56 +0900

    fix tabular

--- vern/parse_tabular.py
+++ vern/parse_tabular.py
@@ -6,7 +6,7 @@ __all__ = ['parse_tabular']
 
 def read_files(input_path, csv_path, plot_path, interactive=False, linear_regression=False):
     df = pd_read_csv( 
-        filename=input_path, encoding="utf-8", sep=", ", header_count=0,
+        filename=input_path, encoding="utf-8", sep=",", header_count=0,
         rename_columns=False,
     )
     df.to_csv(csv_path, index=False)
