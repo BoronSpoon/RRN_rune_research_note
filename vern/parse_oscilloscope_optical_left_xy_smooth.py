bf1b325f55b96ee577967d932088d12829bc84e0
rayanti <rayanticlimatic@gmail.com>
2023-01-26 15:32:35 +0900

    debug filter

--- vern/parse_oscilloscope_optical_left_xy_smooth.py
+++ vern/parse_oscilloscope_optical_left_xy_smooth.py
@@ -28,7 +28,7 @@ def read_files(input_path, txt_path, plot_path, hist_path, interactive=False):
 def smooth(x, y):
     # band pass filter from 100 MHz to 10 GHz
     freq = 1/(x[1] - x[0])
-    sos = signal.butter(100e6/freq, 10e9/freq, 'hp', fs=1000, output='sos')
+    sos = signal.butter(10, [100e6, 10e9], 'hp', fs=freq, output='sos')
     filtered = signal.sosfilt(sos, y)
     return filtered
 
