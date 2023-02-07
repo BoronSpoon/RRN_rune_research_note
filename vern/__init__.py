2f28e238044f755c4ddacf4d9e51a573d473c5c9
rayanti <rayanticlimatic@gmail.com>
2023-02-07 16:09:34 +0900

    update axis

--- vern/__init__.py
+++ vern/__init__.py
@@ -2,7 +2,7 @@ import os, glob
 from .parse_all import *
 
 __copyright__    = 'Copyright (C) 2022 BoronSpoon'
-__version__      = '1.3.7b'
+__version__      = '1.3.8b'
 __license__      = 'MIT License'
 __author__       = 'boronspoon'
 __author_email__ = 'rayanticlimactic@gmail.com'
